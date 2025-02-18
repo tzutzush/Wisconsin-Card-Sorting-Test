f9ae60a41ec6afaaf6a60622c0855b620bf0ff82
unknown <bongyikadam@gmail.com>
2025-02-18 16:08:41 +0100

    Demo initialization UI
    Business logic is to be set up next.

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,22 +1,41 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import Card from "@/components/ui/card";
 import { initialCards } from "@/lib/cards";
 import { CardShape } from "@/lib/types";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Page() {
+  const [demoStarted, setDemoStarted] = useState<boolean>(false);
+  const [randomCard, setRandomCard] = useState<CardShape | null>(null);
+
+  function startDemo() {
+    setDemoStarted(true);
+    setRandomCard({
+      src: "/circles/blue_card_with_1_circles.png",
+      shape: "circle",
+      color: "blue",
+      quantity: 1,
+    });
+  }
+
   return (
-    <div className="flex items-center justify-center h-full flex-col">
+    <div className="flex items-center justify-center h-full flex-col gap-8">
       <div className="flex items-center justify-center gap-5">
         {initialCards.map((card: CardShape) => (
-          <Card
-            key={card.src}
-            src={card.src}
-            shape={card.shape}
-            color={card.color}
-            quantity={card.quantity}
-          />
+          <Card key={card.src} {...card} />
         ))}
       </div>
+      {randomCard ? (
+        <Card {...randomCard} />
+      ) : (
+        <div className="w-44 h-60 bg-white shadow-lg rounded-lg flex items-center justify-center">
+          Not started yet
+        </div>
+      )}
+      <Button disabled={demoStarted} onClick={startDemo}>
+        Start demo
+      </Button>
     </div>
   );
 }
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import Image from "next/image";
 import { CardShape } from "@/lib/types";
 
-export default function Card({ src, shape, color, quantity }: CardShape) {
+export default function Card(card: CardShape) {
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
       <Image
-        src={src}
-        data-shape={shape}
-        data-color={color}
-        data-quantity={quantity}
+        src={card.src}
+        data-shape={card.shape}
+        data-color={card.color}
+        data-quantity={card.quantity}
         alt="Picture of the author"
         width={180}
         height={240}
